7fd30c5d3661c19cf42d653c450c1633af17820c
Tom Herbert <taherbert@gmail.com>
2024-06-22 16:50:36 -0700

    dinkin

--- combine.py
+++ combine.py
@@ -7,11 +7,15 @@ def combine_files(main_file, output_file):
                     filename = line.split('=')[1].strip()
                     # Write contents of the referenced file
                     with open(filename, 'r') as imported_file:
-                        outfile.write(f"# Contents of {filename}\n")
+                        outfile.write(f"\n")
                         outfile.write(imported_file.read())
                         outfile.write("\n")
+                # else if line contains the phrase "Imports" case insensitive remove the entire line and carriage return and continue
+                elif line.lower().startswith('# imports'):
+                    # delete the line
+                    outfile.write(f"")
                 else:
                     outfile.write(line)
 
 # Usage
-combine_files('vengeance.simc', 'vengeance_full.simc')
+combine_files('vengeance/vengeance.simc', 'vengeance/vengeance_full.simc')
